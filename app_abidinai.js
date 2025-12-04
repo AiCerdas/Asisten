2ef45dc4916d1b05d44b3d79164cbfa17e6234a7
AiCerdas <pirnosudar@gmail.com>
2025-12-04 22:36:06 +0700

    Update app_abidinai.js

--- app_abidinai.js
+++ app_abidinai.js
@@ -349,7 +349,9 @@ JANGAB Bicara kurang dari jika menjelaskan dll.
 JANGAN gunakan tanda pagar (#) dalam teks. 
 Semua output harus full HTML.
 
-   - List: <ul><li></li></ul> atau <ol><li></li></ol>
+   - Tebal: <b></b>, Miring: <i></i>, Garis Bawah: <u></u>
+   - Kutipan: <blockquote></blockquote>
+   - Tabel: WAJIB MENGGUNAKAN <table style=".."><tr><th>/<td>...</td></tr></table> yang lengkap.
    
 📜 ATURAN UTAMA SUMBER TEPERCAYA:
 1.  **Akurasi:** Jawab hanya berdasarkan informasi faktual, valid, dan akurat.
